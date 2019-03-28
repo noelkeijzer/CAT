b9ffd36fcd907a5b9c23acfdb445c5b6e5c6afce
joostprins <joost_prins@hotmail.com>
2019-03-28 14:27:36 +0100

    Add working state of mythril-classic

--- analyzer/mythX.py
+++ analyzer/mythX.py
@@ -1,10 +1,21 @@
 from threading import Thread
+from mythril.mythril import Mythril
 
 
-class Main(Thread):
-    def __init__(self):
+class MythX(Thread):
+    def __init__(self, address):
         Thread.__init__(self)
+        self.address = address
 
     def run(self):
-        # start all the different threads from here
+        mythril = Mythril()
+        mythril.set_api_rpc('infura-ropsten')
+
+        report = mythril.fire_lasers('bfs', address=self.address ,contracts=mythril.contracts, address=self.address, max_depth=50, modules=[],
+                                     transaction_count=2)
         pass
+
+
+if __name__ == '__main__':
+    myth_x = MythX('0x3e1225be4e3839245ba76b823cfbc075b89f751a')
+    myth_x.run()

8a1f76a57618a76dce07ed5f85bd382bc525e8f4
joostprins <joost_prins@hotmail.com>
2019-03-28 12:39:48 +0100

    Add skeleton files

---
+++ analyzer/mythX.py
@@ -0,0 +1,10 @@
+from threading import Thread
+
+
+class Main(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        # start all the different threads from here
+        pass

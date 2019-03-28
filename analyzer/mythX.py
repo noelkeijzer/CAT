f2220926e6df544d5d9e26a14037a5b2ebd8e71b
joostprins <joost_prins@hotmail.com>
2019-03-28 15:12:06 +0100

    Modify the analyzer to work in its own thread

--- analyzer/mythX.py
+++ analyzer/mythX.py
@@ -3,17 +3,29 @@ from mythril.mythril import Mythril
 
 
 class MythX(Thread):
-    def __init__(self, address):
+    def __init__(self, new_address_q, report_q, event):
         Thread.__init__(self)
-        self.address = address
+        self.new_address_q = new_address_q
+        self.report_q = report_q
+        self.event = event
+        Thread.daemon = True
 
     def run(self):
-        mythril = Mythril()
-        mythril.set_api_rpc('infura-ropsten')
 
-        report = mythril.fire_lasers('bfs', address=self.address ,contracts=mythril.contracts, address=self.address, max_depth=50, modules=[],
-                                     transaction_count=2)
-        pass
+        while not self.new_address_q.empty():
+            address = self.new_address_q.get()
+
+            if address is None:
+                break
+
+            mythril = Mythril()
+            mythril.set_api_rpc('infura-ropsten')
+            mythril.load_from_address(address=address)
+            report = mythril.fire_lasers('bfs', address=address, contracts=mythril.contracts, max_depth=50, modules=[],
+                                         transaction_count=2, enable_iprof=False)
+
+            self.report_q.put(report)
+            self.new_address_q.task_done()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-class Main():
+from queue import Queue
+from analyzer.mythX import MythX
+from threading import Event
+
+class Main:
     def __init__(self):
         pass
 
-    
+    def main(self):
+        # create different queues
+        new_address_q = Queue()
+        report_q = Queue()
+        stop_event = Event()
+        # create different threads
+        myth_x = MythX(new_address_q, report_q, stop_event)
+        myth_x.start()
+
+        try:
+            stop_event.wait()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            new_address_q.put(None)
+            report_q.put(None)
